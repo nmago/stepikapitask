de47bc66b0967650e29875fa95aad9ce9dc99f0a
Nur-Magomed <nmagoru@gmail.com>
2018-05-06 14:49:39 +0300

    It's working

--- src/main/java/ru/nmago/stepikapitest/Main.java
+++ src/main/java/ru/nmago/stepikapitest/Main.java
@@ -1,7 +1,36 @@
 package ru.nmago.stepikapitest;
 
+import ru.nmago.stepikapitest.api.StepikAPI;
+import ru.nmago.stepikapitest.model.Course;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args){
-        System.out.println("Initial");
+        int N;
+        boolean showProgress;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите N: ");
+        N = scanner.nextInt();
+
+        System.out.println("Выводить прогресс? (y/n): ");
+        showProgress = scanner.next().equals("y");
+
+        System.out.println("Загрузка..");
+        StepikAPI stepikAPI = new StepikAPI(showProgress);
+        List<Course> courses = stepikAPI.getTopNCourses(N);
+
+        System.out.println("== Топ курсов ==");
+        for(int i = 0; i < courses.size(); ++i){
+            Course current = courses.get(i);
+            System.out.printf("Курс #%d\n", i + 1);
+            System.out.printf("Название: %s\n", current.getTitle());
+            System.out.printf("Всего изучающих: %d чел.\n", current.getLearnersCount());
+            System.out.println("- - -");
+        }
     }
+
 }
